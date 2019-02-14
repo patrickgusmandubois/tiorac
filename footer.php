5dad4901c48779d68f0fc4a9b66d06fcf6e29a63
Ricardo Augusto Coelho <tiorac@msn.com>
2019-02-14 20:26:16 -0200

    Footer

--- footer.php
+++ footer.php
@@ -32,26 +32,6 @@
 			}
 		}
 		?>
-		<div class="site-info">
-			<?php $blog_info = get_bloginfo( 'name' ); ?>
-			<?php if ( ! empty( $blog_info ) ) : ?>
-				<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>,
-			<?php endif; ?>
-			
-			<?php if ( has_nav_menu( 'footer' ) ) : ?>
-				<nav class="footer-navigation" aria-label="<?php esc_attr_e( 'Footer Menu', 'black-digital' ); ?>">
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'footer',
-							'menu_class'     => 'footer-menu',
-							'depth'          => 1,
-						)
-					);
-					?>
-				</nav><!-- .footer-navigation -->
-			<?php endif; ?>
-		</div><!-- .site-info -->
 	</footer><!-- .site-footer -->
 
 </div><!-- #page -->
