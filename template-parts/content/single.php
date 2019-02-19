048c752aee93d4003c7a96e640df85ac46bd0802
Ricardo Coelho <tiorac@msn.com>
2019-02-19 15:31:43 -0300

    Remoção title

--- template-parts/content/single.php
+++ template-parts/content/single.php
@@ -12,24 +12,5 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_sticky() && is_home() && ! is_paged() ) {
-			
-		}
 
-		the_title( '<h1 class="entry-title">', '</h1>' );
-		?>
-	</header><!-- .entry-header -->
-
-
-	<div class="entry-content">
-		<?php
-		the_content();
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		
-	</footer><!-- .entry-footer -->
 </article><!-- #post-${ID} -->
