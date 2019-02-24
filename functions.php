76dd4ee0c1a69bb904365e06f6f86be2a370686d
Ricardo Augusto Coelho <tiorac@msn.com>
2019-02-24 12:35:33 -0300

    ok

--- functions.php
+++ functions.php
@@ -178,10 +178,7 @@ add_action( 'wp_print_footer_scripts', 'twentynineteen_skip_link_focus_fix' );
 */
 
 function blackdigital_get_the_archive_title($title) {
-
-	asdsadsadsad
-
-    if ( is_category() ) {
+	if ( is_category() ) {
 		$title = single_cat_title( '', false );
 	} elseif ( is_tag() ) {
 		$title = single_tag_title( '', false );
